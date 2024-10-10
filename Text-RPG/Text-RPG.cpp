fd1896ee54260f7a44d5c86e2ef622e911456578
561fffDemon <76696800+561fffDemon@users.noreply.github.com>
2024-10-10 20:36:27 +0400

    Updated engine
    Added display
    AHHHHHHH

--- Text-RPG/Text-RPG.cpp
+++ Text-RPG/Text-RPG.cpp
@@ -1,6 +1,14 @@
 ﻿#include <iostream>
 #include "engine.h"
 
-int main(){
+using namespace std;
 
+int main(){
+	setlocale(LC_ALL,"Russian");
+	const int len = 4;
+	string test_options[len]{"Настройки", "Начать игру", "Квесты", "Выход"};
+	display(test_options,len);
+	cout << endl;
+	string test_options1[len]{ "Settings", "Start game", "Quests", "Quit" };
+	display(test_options1, len);
 }
--- Text-RPG/engine.h
+++ Text-RPG/engine.h
@@ -1,2 +1,68 @@
 #pragma once
+#include <iostream>
+#include <windows.h>
+using namespace std;
 
+int getTerminalSize_WIDTH() {
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    int columns;
+
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+    return columns;
+}
+
+int getTerminalSize_HEIGHT() {
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    int rows;
+
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    rows = csbi.srWindow.Right - csbi.srWindow.Top + 1;
+    return rows;
+}
+
+void margin(int len,char fill) {
+    for (int i = 0; i < len; i++) {
+        cout << fill;
+    }
+}
+void split(int len,string* options, char fill) {
+    cout << "|";
+    for (int i = 0; i < len; i++) {
+        margin(options[i].length() + 2, fill);
+    }
+    for (int i = 0; i < len-1; i++) {
+        cout << fill;
+    }
+    cout << "|";
+}
+// 120x30
+
+int countStrings(string* options, int len, int offset) {
+    int sum = 0;
+    for (int i = 0; i < len; i++) {
+        sum = sum + options[i].length() - offset;
+    }
+    return sum;
+}
+
+void display(string* options,int len) {
+    int consolewidth = getTerminalSize_WIDTH();
+    int uiTextSize = countStrings(options,len,3);
+    int medium = (int)((consolewidth / 2) - (uiTextSize / 2));
+
+    for (int i = 0; i < len; i++) {
+        medium -= options[i].length() - 3;
+    }
+    margin(medium, ' ');
+    split(len, options, '-');
+    cout << endl;
+    margin(medium, ' ');
+    cout << "| ";
+    for (int i = 0; i < len; i++) {
+        cout << options[i] << " | ";
+    }
+    cout << endl;
+    margin(medium, ' ');
+    split(len, options, '-');
+}
